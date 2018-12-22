2e6fa40e5e496e5fa11572a5e75e0f96ec875b8f
ios <ios@iosdemacbook-pro.local>
2018-12-22 15:28:38 +0800

    Initial Commit

---
+++ AMCProject/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  AMCProject
+//
+//  Created by ios on 2018/12/22.
+//  Copyright © 2018 ios. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
