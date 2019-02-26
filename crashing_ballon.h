1a04a222f8de73ad66501daf42bdf97d8de35480
Anice L <rcetangle@qq.com>
2019-02-26 20:00:01 +0800

    Update crashing_ballon.h

--- crashing_ballon.h
+++ crashing_ballon.h
@@ -13,8 +13,8 @@
 #include <string.h>
 #include <stdlib.h>
 
+#define PRINT_FLAG 1
 #define CRASH_MAX_CNT 100
-
 typedef struct {
     int data[CRASH_MAX_CNT];
     int current;
@@ -67,7 +67,7 @@ int recursion(DATA* data, const int target, const int* except) {
         }
         data->initial = 1;
         data->len--;
-        if (data->len > 1)
+        if (data->len >= 1)
             ret = recursion(data, target, except);
     }
 
@@ -79,7 +79,9 @@ int calculateMods(const int a, const int b) {
     int needInitial = 1;
     DATA* now = NULL;
     int lenB = 0;
-    while (ret == 2) {
+    int i = 1;
+    while (1 == i) {
+        i++;
         if (1 == needInitial) {
             if (now != NULL) {
                 free(now);
@@ -96,7 +98,9 @@ int calculateMods(const int a, const int b) {
             if (1 == recursion(now, b, NULL)) {
                 if (now->data[0] > 1) break;
                 lenB++;
+#ifdef PRINT_FLAG
                 printData(now);
+#endif
                 DATA* dtA = (DATA*)malloc(sizeof(DATA));
                 dtA->len = 0;
                 dtA->data[0] = 0;
@@ -106,32 +110,38 @@ int calculateMods(const int a, const int b) {
                 int except[CRASH_MAX_CNT];
                 memset(except, 0, sizeof(int)*CRASH_MAX_CNT);
                 int j;
-                for (j = 0; j < now->len; j++) {
-                    if (now->len == 0) {
-                        except[0] = 1;
-                    } else {
+                for (j = 1; j < now->len; j++) {
+//                    if (now->len == 0) {
+//                        except[0] = 1;
+//                    } else {
                         except[now->data[j]-1] = 1;
-                    }
+//                    }
                 }
                 if (1 == recursion(dtA, a, except)) {
-//                    printf("b使用掉了因子之后，还能乘出a，证明a说真的\n");
-//                    printData(dtA);
+#ifdef PRINT_FLAG
+                    printf("b使用掉了因子之后，还能乘出a，证明a说真的\n");
+                    printData(dtA);
+#endif
                     ret = 1;
                 }
                 free(dtA);
                 
-                if (ret == 2 && now->len > 2) {
+                if (now->len > 2) {
+                    i--;
                     needInitial = 0;
                     DATA* next = (DATA*)malloc(sizeof(DATA));
                     next->len = now->len-2;
                     next->initial = 1;
                     memcpy(next->data, now->data, sizeof(int)*now->len-1);
                     next->current = now->current/(now->data[now->len-1]*now->data[now->len-2]);
+                    free(now);
                     now = next;
                 }
             } else {
                 if (lenB == 0) {
-//                    printf("b没有任何因子\n");
+#ifdef PRINT_FLAG
+                    printf("b没有任何因子\n");
+#endif
                     ret = 1;
                 }
             }
@@ -161,4 +171,37 @@ void crashing_ballon() {
     }
 }
 
+// 别人家的
+int flagA,flagB;
+void dfs(int m,int n,int kk)    //运用了深度优先 的搜索策略
+{
+    int k =kk;
+    if(m==1 && n==1)    //在两个数的所有各不相同的因子中，有因子能重新乘出给出的两个数，则A说了真话
+    {
+        flagA=1; //A说了真话
+        return ;
+    }
+    if(n==1) //在两个数的所有各不相同的因子中，没有任何因子能重新乘出给出的两个数，则B说了真话
+        flagB =1;   //B说了真话
+    while( (k <  m || k <  n) && (k< 100) )
+    {
+        k++;
+        /*
+         *依次找出两个数所有各不相同的因子，如24和12的所有因子为 2，3，4，6，8，12 ，
+         再在这些因子中搜索，看是否能重新乘出给出的两个数
+         */
+        if(m%k ==0)
+        {
+            dfs(m/k,n,k);
+            if(flagA)
+                return ;
+        }
+        if(n%k ==0 )
+        {
+            dfs(m,n/k,k);
+            if(flagA)
+                return ;
+        }
+    }
+}
 #endif /* crashing_ballon_h */
