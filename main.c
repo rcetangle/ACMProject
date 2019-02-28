80f1950d87debb745c8d1520593701167cf207f4
Anice L <rcetangle@qq.com>
2019-02-28 14:50:52 +0800

    Update main.c

--- main.c
+++ main.c
@@ -9,26 +9,33 @@
 #include <stdio.h>
 #include <time.h>
 
+#include "main.h"
 #include "a_plus_b.h"
 #include "html.h"
 #include "crashing_ballon.h"
+#include "anagrams_by_stack.h"
+#include "jugs.h"
 
 void problemFactory(const int type) {
-   switch (type) {
-       case 1:
-           a_plus_b();
-           break;
-       case 2:
-           html();
-       case 3:
-           crashing_ballon();
-       default:
-           break;
-   }
+    switch (type) {
+        case 1:
+            a_plus_b();
+            break;
+        case 2:
+            html();
+        case 3:
+            crashing_ballon();
+        case 4:
+            anagrams_by_stack();
+        case 5:
+            jugs();
+        default:
+            break;
+    }
 }
 
 int main(int argc, const char * argv[]) {
-   // insert code here...
-   problemFactory(3);
-   return 0;
+    // insert code here...
+    problemFactory(5);
+    return 0;
 }
