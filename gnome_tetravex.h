0fde2d54ac77fc1262383beb16ed9c3a392796de
Anice L <rcetangle@qq.com>
2019-03-05 18:51:32 +0800

    Update gnome_tetravex.h

--- gnome_tetravex.h
+++ gnome_tetravex.h
@@ -10,8 +10,8 @@
 #define gnome_tetravex_h
 
 typedef struct square {
-    int datas[4]; /* top right bottom left*/
-    struct square* adjoins[4]; /* top right bottom left*/
+    int datas[4]; /* top right bottom left */
+    struct square* adjoins[4]; /* pointer of top right bottom left square */
 } Square;
 
 void printSquares(Square** squares, const int n) {
@@ -30,26 +30,103 @@ void printSquares(Square** squares, const int n) {
         for (i = 0; i < n; i++) {
             printf("|\t%d\t | ", squares[i+j*n]->datas[2]);
         }
-        printf("\n\n");
+        printf("\n");
+        for (i = 0; i < n; i++) {
+            printf("%p\t", squares[i+j*n]);
+        }
+        printf("\n");
     }
 #endif
 }
 
+void markAsHorizentalSquare(Square* a, Square* b) {
+    if (a->adjoins[0] == b || b->adjoins[0] == a) return; /* a b已经存在垂直关系 */
+    if (a->datas[1] == b->datas[3] && a->adjoins[1] == NULL && b->adjoins[3] == NULL) { /* b在a的右边 */
+        a->adjoins[1] = b;
+        b->adjoins[3] = a;
+    } else if (a->datas[3] == b->datas[1] && a->adjoins[3] == NULL && b->adjoins[1] == NULL) { /* a在b的右边 */
+        a->adjoins[3] = b;
+        b->adjoins[1] = a;
+    }
+}
+
+void markAsVerticalSquare(Square* a, Square* b) {
+    if (a->adjoins[1] == b || b->adjoins[1] == a) return; /* a b已经存在水平关系 */
+    if (a->datas[2] == b->datas[0] && a->adjoins[2] == NULL && b->adjoins[0] == NULL) { /* b在a的下边 */
+        a->adjoins[2] = b;
+        b->adjoins[0] = a;
+    } else if (a->datas[0] == b->datas[2] && a->adjoins[0] == NULL && b->adjoins[2] == NULL) { /* a在b的下边 */
+        a->adjoins[0] = b;
+        b->adjoins[2] = a;
+    }
+}
+
 void findSquares(Square** squares, const int n) {
-    int i = 0;
-    Square* sq = squares[0];
-    for (i = 1; i < n*n; i++) {
-        Square* sq2 = squares[i];
-        if (sq->adjoins[0] == NULL && sq->datas[0] == sq2->datas[2]) { /* sq2在sq1s上边 */
-            sq->adjoins[0] = sq2;
-        } else if (sq->adjoins[1] == NULL && sq->datas[1] == sq2->datas[3]) { /* sq2在sq1右边 */
-            sq->adjoins[1] = sq2;
-        } else if (sq->adjoins[2] == NULL && sq->datas[2] == sq2->datas[0]) { /* sq2在sq1s下边 */
-            sq->adjoins[2] = sq2;
-        }  else if (sq->adjoins[3] == NULL && sq->datas[3] == sq2->datas[1]) { /* sq2在sq1右边 */
-            sq->adjoins[3] = sq2;
+    int i, j;
+    for (j = 0; j < n*n-1; j++) {
+        Square* sq = squares[j];
+        for (i = j+1; i < n*n; i++) {
+            Square* sq2 = squares[i];
+            markAsHorizentalSquare(sq, sq2); /* 两个正方形的水平关系 */
+            markAsVerticalSquare(sq, sq2); /* 两个正方形的垂直关系 */
         }
     }
+}
+
+/* 获取头部 */
+Square* getHead(Square** squares, const int n) {
+    Square* next = squares[0];
+    Square* head = NULL;
+    while (head == NULL) {
+        if (next->adjoins[0] == NULL && next->adjoins[3] == NULL) {
+            head = next;
+        } else {
+            next = next->adjoins[0] == NULL? next->adjoins[3] : next->adjoins[0];
+        }
+    }
+    return head;
+}
+
+void printSquares2(Square** squares, const int n) {
+#ifdef PRINT_FLAG
+    Square* head = getHead(squares, n);
+
+    printf("\n---------- square 2 ----------\n");
+    Square* next = head;
+    Square* rowHead = NULL;
+    char tops[1000];
+    char middles[1000];
+    char bottom[1000];
+    memset(tops, '\0', 1000*sizeof(char));
+    memset(middles, '\0', 1000*sizeof(char));
+    memset(bottom, '\0', 1000*sizeof(char));
+    while (next != NULL) {
+        if (next->adjoins[3] == NULL) {
+            rowHead = next;
+            printf("%s\n%s\n%s\n\n", tops, middles, bottom);
+            memset(tops, '\0', 255*sizeof(char));
+            memset(middles, '\0', 255*sizeof(char));
+            memset(bottom, '\0', 255*sizeof(char));
+        }
+        if (next != NULL) {
+            char tmps[20];
+            sprintf(tmps,"|\t%d\t | ", next->datas[0]);
+            strcat(tops, tmps);
+            sprintf(tmps,"|%d\t\t%d| ", next->datas[3], next->datas[1]);
+            strcat(middles, tmps);
+            sprintf(tmps,"|\t%d\t | ", next->datas[2]);
+            strcat(bottom, tmps);
+        }
+        next = next->adjoins[1];
+        if (next == NULL) {
+            next = rowHead->adjoins[2]; // 下一行了
+            while (next != NULL && next->adjoins[3] != NULL) {
+                next = next->adjoins[3];
+            }
+        }
+    }
+    printf("%s\n%s\n%s\n\n", tops, middles, bottom);
+#endif
 }
 
 void gnome_tetravex() {
@@ -62,6 +139,7 @@ void gnome_tetravex() {
         scanf("%d", &n);
         if (n == 0) break;
         Square** squares = (Square**)malloc(sizeof(Square*)*n*n);
+        memset(squares, 0, sizeof(Square*)*n*n);
 #ifdef PRINT_FLAG
         printf("input %d ints(0~9) for triangles:\n", n*n*4);
 #endif
@@ -69,9 +147,7 @@ void gnome_tetravex() {
         for (i = 0; i < n; i++) {
             for (j = 0; j < n; j++) {
                 Square* sq = (Square*)malloc(sizeof(Square));
-                int k;
-                for (k = 0; k < 4; k++)
-                    sq->adjoins[k] = NULL;
+                memset(sq->adjoins, 0, sizeof(Square*)*4);
                 scanf("%d %d %d %d", &sq->datas[0], &sq->datas[1], &sq->datas[2], &sq->datas[3]);
                 squares[i*n+j] = sq;
             }
@@ -79,10 +155,7 @@ void gnome_tetravex() {
         printSquares(squares, n);
         
         findSquares(squares, n);
-//        horizentalSwap(squares, n*n);
-//        printSquares(squares, n);
-//        verticalSwap(squares, n*n);
-//        printSquares(squares, n);
+        printSquares2(squares, n);
     }
 }
 #endif /* gnome_tetravex_h */
