972a11baaf5727d62adc2e69d9f3152f3ea894d6
Anice Liao <Anice.air@163.com>
2019-03-12 21:56:29 +0800

    main4Submit

---
+++ main4Submit.c
@@ -0,0 +1,12 @@
+/*
+ * this is file for sumbiting to ZOJ
+ */
+
+#include <stdio.h>
+#include <time.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(int argc, const char * argv[]) {
+    return 0;
+}
