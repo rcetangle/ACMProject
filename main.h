0672ddf43eb62ac0efbd166e5b0150ec7c9fa0cd
Anice L <rcetangle@qq.com>
2019-04-03 15:08:11 +0800

    Update main.h

--- main.h
+++ main.h
@@ -11,4 +11,32 @@
 
 #define PRINT_FLAG 1
 
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int mem_cnt = 0;
+void * safe_malloc(size_t size)
+{
+    void * mem = malloc(size);
+    if(mem)
+    {
+        mem_cnt++;
+    }
+    return mem;
+}
+
+void safe_free(void * ptr)
+{
+    if(ptr == NULL || mem_cnt == 0) return;
+    free(ptr);
+    ptr = NULL;
+    mem_cnt--;
+}
+
+void mem_leak_result()
+{
+    printf("\ncheck memory leak %d\n", mem_cnt);
+}
+
 #endif /* main_h */
